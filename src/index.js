2b4e20cbd2638ab4cd68628fdf8515bc3ef0f747
Kristen <kristen.l.lutz@gmail.com>
2017-11-19 15:46:00 -0500

    playing around with elements

--- src/index.js
+++ src/index.js
@@ -2,7 +2,35 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(
-  <App cat={5} txt="test" />,
-  document.getElementById('root')
-);
+const square = React.createElement('div', {
+  style: {
+    width: '100px',
+    height: '100px',
+    backgroundColor: 'red',
+    margin: '10px',
+  }
+})
+const blueSquare = React.createElement('div', {
+  style: {
+    width: '100px',
+    height: '100px',
+    backgroundColor: 'blue',
+    margin: '10px',
+  }
+})
+
+
+const container = React.createElement(
+  // The first argument specifies the element's type
+  'div',
+
+  // The second argument specifies the element's attributes, or "props"
+  { style: { border: '5px solid blue' } },
+
+  // The remaining arguments list the element's children
+  square,
+  square,
+  blueSquare
+)
+
+ReactDOM.render(container, document.getElementById('root'))
