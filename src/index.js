916611954e15de140056d5c723ec56cc018b804c
Kristen <kristen.l.lutz@gmail.com>
2017-11-19 19:21:44 -0500

    pythagoras tree test

--- src/index.js
+++ src/index.js
@@ -2,32 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-import { getBoxStyle } from './Spiral.js'
-
-
-
-var boxes2 = [];
-for (var i=0; i<=15;i++) {
-	if((i+1)%3===0&&(i+1)%5===0){
-		boxes2.push(React.createElement('div', { style: getBoxStyle(i), key: i }, 
-				React.createElement('strong', {}, 'FizzBuzz'))
-		)
-	}else if((i+1)%3===0){
-		boxes2.push(React.createElement('div', { style: getBoxStyle(i), key: i }, 
-				React.createElement('strong', {}, 'Fizz'))
-		)
-	}else if((i+1)%5===0){
-		boxes2.push(React.createElement('div', { style: getBoxStyle(i), key: i }, 
-				React.createElement('strong', {}, 'Buzz'))
-		)
-	}else{
-		boxes2.push(React.createElement('div', { style: getBoxStyle(i), key: i }, i+1))
-
-	}
-}
+import { PythagorasTree } from './PythagorasTree.js';
 
 ReactDOM.render(
-  React.createElement('div', {}, boxes2),
+  React.createElement(PythagorasTree, {
+    sprout: 0.01,
+    sway: 0.04,
+  }),
   document.getElementById('app')
 )
 
